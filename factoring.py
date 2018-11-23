e5e97f09b8fe44a0fd1ebc0ec26eada44f5f5244
Daniel <daniel.dimitric91@gmail.com>
2018-11-23 15:45:44 +0100

    clean up

--- factoring.py
+++ factoring.py
@@ -26,7 +26,7 @@ def get_primes(n):
         i += 1
 
 
-def prime_facs3(n):
+def prime_facs(n):
     factors = []
     for prime in prime_fac_base:
         if n == 1:
@@ -38,43 +38,6 @@ def prime_facs3(n):
     if n > 1:
         return False
     return factors
-
-
-def prime_facs2(n):
-    step = 1
-    i = 2
-    if(i > 2):
-        step = 2
-    factors = []
-    while i * i <= n:
-        if (i > largest_factor_prime):
-            return False
-        if n % i:
-            i += step
-        else:
-            n //= i
-            factors.append(i)
-    if n > 1 and n in prime_fac_base:
-        factors.append(n)
-    else:
-        return False
-    return factors
-
-
-def prime_facs(r):
-    factors = []
-    factor_base = prime_fac_base
-    for prim_nbr in factor_base:
-        while r % prim_nbr == 0:
-            r = r // prim_nbr
-            factors.append(prim_nbr)
-    if r > 1 and r in factor_base:
-        factors.append(r)
-    return factors
-
-
-def is_smooth(prim):
-    return prim <= largest_factor_prime
 
 
 def guess_r(a, b):
@@ -99,23 +62,6 @@ string_to_input = str(L) + " " + str(F) + "\n"
 
 prime_fac_base = get_primes(F)
 largest_factor_prime = prime_fac_base[-1]
-
-
-# rone = guess_r(3, 2)
-# rtwo = guess_r(4, 4)
-# rthree = guess_r(5, 3)
-# rfour = guess_r(5, 4)
-# rfive = guess_r(6, 2)
-# rsix = guess_r(7, 2)
-# rseven = guess_r(10, 6)
-# reight = guess_r(11, 4)
-# rnine = guess_r(12, 12)
-# rten = guess_r(13, 4)
-# releven = guess_r(13, 8)
-# rtwelve = guess_r(14, 8)
-# ad = guess_r(14, 8)
-# r_list_test = [rone, rtwo, rthree, rfour, rfive, rsix, rseven, reight, rnine, rten, releven, rtwelve, ad]
-
 saved_r_vals = []
 binary_m = set()
 sum = 2
@@ -129,9 +75,9 @@ while len(binary_m) < L:
         r_to_save = r
         r = r**2 % N
         if r > 1:
-            r_primes = prime_facs3(r)
+            r_primes = prime_facs(r)
             if r_primes:
-                # find out count of exponents
+                # Find out count of exponents
                 count = dict(Counter(r_primes))
                 # Do binary row:
                 binary_row = [0] * len(prime_fac_base)
@@ -153,16 +99,9 @@ while len(binary_m) < L:
                         else:
                             binary_string = binary_string + " " + str(x)
                     string_to_input = string_to_input + binary_string + "\n"
-                    saved_r_vals.append([index, r_to_save, r_primes, binary_row])
+                    saved_r_vals.append([index, r_to_save, r_primes])
                     index = index + 1
-                # print(count)
-                # print(binary_row)
-                # print(prime_fac_base)
-                #
-                # print(binary_m)
-                # print(len(binary_m))
-                # print(saved_r_vals)
-                # print(string_to_input)
+                # Prints to see progress
                 print(index)
                 print("---------------------------------")
         sum = sum + 1
@@ -174,41 +113,32 @@ input_file.close()
 path = os.getcwd()
 path = path + "/a.out input.txt output.txt"
 os.system(path)
+
 # Read in solutions
 with open("output.txt") as f:
     lines = f.readlines()
 lines = [x.strip() for x in lines]
 lines = lines[1:]
-# print("lines", len(lines), lines)
+
 for line in lines:
     indexes = []
     line = re.sub(r"\s", "", line)
-    # print(line)
     for i in range(len(line)):
         if line[i] == "1":
             indexes.append(i)
-    # print("indexes", indexes)
     # Test each solution now
     sum_r = 1
     sum_r_2 = 1
     list_r_2 = []
     for i in indexes:
-        # print(saved_r_vals[i][1])
         sum_r = (sum_r * saved_r_vals[i][1]) % N
         list_r_2.extend(saved_r_vals[i][2])
     list_r_2.sort()
     count_r_2 = dict(Counter(list_r_2))
     copy = deepcopy(count_r_2)
-    # print(count_r_2)
-    # tuple_r_2 = []
     for k, v in copy.items():
-        # tuple_r_2.append([k, int(v/2)])
         temp = [k, int(v/2)]
-        # print(temp)
         sum_r_2 = (sum_r_2 * (temp[0]**temp[1])) % N
-
-    # print(sum_r)
-    # print(sum_r_2)
     fac_1 = gcd(abs(sum_r_2 - sum_r), N)
     if fac_1 != 1 and fac_1 != N:
         fac_2 = int(N/fac_1)
